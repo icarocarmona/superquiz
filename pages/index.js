7c1a9e213ce21b88a3779537e217000e4b81905e
Icaro Carmona <icarocarmona@hotmail.com>
2021-01-26 18:07:37 -0300

    add eslint

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,6 @@ import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
-
-
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
@@ -29,7 +21,6 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
   const router = useRouter();
   const [name, setName] = React.useState('');
@@ -37,20 +28,21 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>AluraQuiz - Modelo Base</title>
+        <title>Big Data Quiz</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>The legend of zelda</h1>
+            <h1>Big Data</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (infosDoEvento) {
-              infosDoEvento.preventDefault();
-              router.push(`/quiz?name=${name}`);
-              console.log('Fazendo uma submissão por meio do react');
-            }}
+            <form
+              onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                console.log('Fazendo uma submissão por meio do react');
+              }}
             >
               <input
                 onChange={function (infosDoEvento) {
@@ -73,7 +65,7 @@ export default function Home() {
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <p>Quiz para testar seu conhecimento em Big Data</p>
           </Widget.Content>
         </Widget>
         <Footer />
---
+++ pages/quiz.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function QuizPageTeste() {
+  return <div>Pagina Raiz</div>;
+}
