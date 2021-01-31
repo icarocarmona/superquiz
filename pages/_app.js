61785c214fbeb51be7773f93a694259a49b84340
Icaro Carmona <icarocarmona@hotmail.com>
2021-01-31 00:07:26 -0300

    add meta og:image and images

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,10 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content="Teste seu conhecimento sobre Big Data" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <meta
+          property="og:image"
+          content="https://supertechquiz.vercel.app/big.png"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
           rel="stylesheet"
